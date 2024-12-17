c59266f5c743a3f3d1fa82eb3f2f20a79ba18d51
Jalbrin445 <mezaguzmanjuanalbrin@gmail.com>
2024-12-17 13:31:57 -0500

    Solución a nuevos errores de sonidos e imagen

--- background_play.py
+++ background_play.py
@@ -7,7 +7,7 @@ import utils
 
 # fondo
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-fondo = utils.load_image(os.path.join(BASE_DIR,"\\Users\\Juan\\Desktop\\AVBioquest\\Prototipo7_UsoPygameAndSys\\images\\cielo.png"))
+fondo = utils.load_image(os.path.join(BASE_DIR,"assets\\images\\cielo.png"))
 fondo = pygame.transform.scale(fondo, (settings.SCREEN_WIDTH, settings.SCREEN_HEIGHT*2))
 
 #posición inicial del fondo
--- music.py
+++ music.py
@@ -7,11 +7,12 @@ import os
 pygame.init()
 pygame.mixer.init() # Carga de sonidos
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 # Función para cambiar musica
 def cambiar_musica(nueva_cancion, bucle=True):
     pygame.mixer.music.stop()
-    pygame.mixer.music.load(nueva_cancion)
+    pygame.mixer.music.load(os.path.join(BASE_DIR, nueva_cancion))
     pygame.mixer.music.play(-1 if bucle else 0)
 
 # Musica
@@ -19,6 +20,5 @@ musica_inicio = "assets\\sounds\\A_Binary_Collision.mp3" # sonido menú de inici
 musica_juego = "assets\\sounds\\pantera_rosa.mp3" # sonido en estado playing
 # sonido_colision = pygame.mixer.Sound("\\Users\\Juan\\Desktop\\AVBioquest\\Prototipo6_UsoPygameAndSys\\Game_Over_Sound_Effect.mp3") # sonido de colisión
 musica_gameover = "assets\\sounds\\Game_Over_Sound_Effect.mp3" # sonido game over
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sonido_colision = pygame.mixer.Sound(os.path.join(BASE_DIR, "assets\\sounds\\collision.mp3"))
 
