46178db8d23d83eb3f5da2ffc9d66811bb09bfc9
Jalbrin445 <mezaguzmanjuanalbrin@gmail.com>
2024-12-17 13:50:55 -0500

    Corrección de errores de sonidos

--- music.py
+++ music.py
@@ -12,13 +12,13 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 # Función para cambiar musica
 def cambiar_musica(nueva_cancion, bucle=True):
     pygame.mixer.music.stop()
-    pygame.mixer.music.load(os.path.join(BASE_DIR, nueva_cancion))
+    pygame.mixer.music.load(nueva_cancion)
     pygame.mixer.music.play(-1 if bucle else 0)
 
 # Musica
-musica_inicio = "assets\\sounds\\A_Binary_Collision.mp3" # sonido menú de inicio de juego
-musica_juego = "assets\\sounds\\pantera_rosa.mp3" # sonido en estado playing
+musica_inicio = os.path.join(BASE_DIR,"assets\\sounds\\A_Binary_Collision.mp3") # sonido menú de inicio de juego
+musica_juego = os.path.join(BASE_DIR,"assets\\sounds\\pantera_rosa.mp3") # sonido en estado playing
 # sonido_colision = pygame.mixer.Sound("\\Users\\Juan\\Desktop\\AVBioquest\\Prototipo6_UsoPygameAndSys\\Game_Over_Sound_Effect.mp3") # sonido de colisión
-musica_gameover = "assets\\sounds\\Game_Over_Sound_Effect.mp3" # sonido game over
+musica_gameover = os.path.join(BASE_DIR,"assets\\sounds\\Game_Over_Sound_Effect.mp3") # sonido game over
 sonido_colision = pygame.mixer.Sound(os.path.join(BASE_DIR, "assets\\sounds\\collision.mp3"))
 
